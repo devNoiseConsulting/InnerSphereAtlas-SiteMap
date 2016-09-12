4078dbed3f64c653a59bfc0ed7472d292de4010b
Michael J. Flynn <flynnmj@devnoise.com>
2016-09-12 12:56:02 -0400

    Code cleaning
    Changes to make the code generate a sitemap as close to the one pushed
    to the website. The star map links are the same, but listed in a
    different order.

--- bin/sitemap.sh
+++ bin/sitemap.sh
@@ -13,7 +13,7 @@ cat << FOOTER > footer.xml
 </urlset>
 FOOTER
 
-perl -pe 's/^([0-9]*)\t([0-9\.]*)$/<url><loc>https:\/\/isatlas.teamspam.net\/planet-detail.php?planet=$1<\/loc><priority>$2<\/priority><\/url>\n<url><loc>https:\/\/isatlas.teamspam.net\/planet-detail.php?planet=$1\&amp;amp=1<\/loc><priority>$2<\/priority><\/url>/g' planet.txt > planet.xml
+perl -pe 's/^([0-9]*)\t([0-9\.]*)$/<url><loc>https:\/\/isatlas.teamspam.net\/planet-detail.php?planet=$1<\/loc><priority>$2<\/priority><\/url>\n<url><loc>https:\/\/isatlas.teamspam.net\/planet-detail.php?planet=$1\&amp;mobile=1<\/loc><priority>$2<\/priority><\/url>/g' planet.txt > planet.xml
 
 perl -pe 's/^([0-9]*)$/<url><loc>https:\/\/isatlas.teamspam.net\/novel-detail.php?novel=\1<\/loc><priority>0.6<\/priority><\/url>/g' novel.txt > novel.xml
 
@@ -30,10 +30,6 @@ perl -pe 's/^([0-9]*)\t([0-9\.]*)$/<url><loc>https:\/\/isatlas.teamspam.net\/sys
 perl -pe 's/^([0-9]*)\t([0-9\.]*)$/<url><loc>https:\/\/isatlas.teamspam.net\/system-map.php?planet=\1\&amp;era=3057<\/loc><priority>\2<\/priority><\/url>/g' star_maps.txt >> star_maps.xml
 perl -pe 's/^([0-9]*)\t([0-9\.]*)$/<url><loc>https:\/\/isatlas.teamspam.net\/system-map.php?planet=\1\&amp;era=3062<\/loc><priority>\2<\/priority><\/url>/g' star_maps.txt >> star_maps.xml
 
+cat header.xml planet.xml star_maps.xml novel.xml factory.xml product_type.xml footer.xml > sitemap.xml
 
-cat header.xml planet.xml novel.xml factory.xml product_type.xml star_maps.xml footer.xml > sitemap.tmp.xml
-
-#perl -pe 's/00+<\/priority/0<\/priority/g' sitemap.tmp.xml >> sitemap.xml
-#rm sitemap.tmp.xml
-
-mv sitemap.tmp.xml sitemap.xml
+perl -pe 's/00+<\/priority/0<\/priority/g' -i sitemap.xml
