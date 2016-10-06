f99f63dbc31084ac06284ac2739c182428cd578a
Michael J. Flynn <flynnmj@devnoise.com>
2016-10-06 16:00:13 -0400

    Home page
    Added the home page url to the sitemap.xml file

--- bin/sitemap.sh
+++ bin/sitemap.sh
@@ -7,6 +7,7 @@ rm *.xml > /dev/null
 cat << HEADER > header.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url><loc>https://isatlas.teamspam.net/</loc><priority>1.0</priority></url>
 HEADER
 
 cat << FOOTER > footer.xml
